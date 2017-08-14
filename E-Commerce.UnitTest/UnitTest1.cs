b16df57ffda5e25a71afc65e241da306cde761b5
emerson.costa <emerson.costa@NO0008>
2017-08-14 10:14:44 -0300

    Add project files.

---
+++ E-Commerce.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace E_Commerce.UnitTest
+{
+    [TestClass]
+    public class UnitTestECommerce
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
